af8fce7ffd7bc772494dc129d4739f833e6ab0ff
Ketan Bhokray <kbhokray@chainworks.io>
2022-06-15 18:48:39 +0530

    DeviceOrientationCamera

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+ import {SafeAreaView, useWindowDimensions, View} from 'react-native';
+ import {EngineView, useEngine} from '@babylonjs/react-native';
+ import {Scene, SceneLoader, DeviceOrientationCamera, Vector3, FreeCamera} from '@babylonjs/core';
+ import '@babylonjs/loaders/glTF';
+ 
+ const EngineScreen = (props) => {
+   const engine = useEngine();
+   const [camera, setCamera] = useState();
+   useEffect(() => {
+     if (engine) {
+       const scene = new Scene(engine)
+       scene.createDefaultLight()
+       const url =
+         'https://github.com/KhronosGroup/glTF-Sample-Models/blob/master/2.0/Duck/glTF-Binary/Duck.glb?raw=true';
+       SceneLoader.Append("", url, scene, (sc => {
+        let cam;
+        try {
+          // When FreeCamera is used, the scene renders fine
+          // cam = new FreeCamera('FreeCamera', new Vector3(-10, 1, 1), sc);
+          // cam.setTarget(new Vector3(0, 0, 0))
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+          // When DeviceOrientationCamera is used, it throws an error
+          cam = new DeviceOrientationCamera('DeviceOrientationCamera', new Vector3(10, 1, 1), sc);
+        } catch (err) {
+          console.log(err);
+          throw err;
+        }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        const canvas = engine.getRenderingCanvas();
+        cam.attachControl(canvas, true);  
+        setCamera(sc.activeCamera);
+       }));
+     }
+   }, [engine]);
+ 
+   return (
+     <>
+       <View style={props.style}>
+         <View style={{flex: 1}}>
+           <EngineView camera={camera} displayFrameRate={true} />
+         </View>
+       </View>
+     </>
+   );
+ };
+ 
+ const App = () => {
+   const {width, height} = useWindowDimensions();
+   return (
+     <>
+       <SafeAreaView
+         style={{
+           flex: 1,
+           backgroundColor: 'red',
+           height,
+           width,
+         }}>
+         <EngineScreen style={{flex: 1}} />
+       </SafeAreaView>
+     </>
+   );
+ };
+ 
+ export default App;
+ 
